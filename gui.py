9dd062430fb040d274451ee2668f31067cee6c34
skl.progs <skl.progs@gmail.com>
2018-01-02 21:29:26 +0300

    Channel.center; Channel.update_scroll

--- gui.py
+++ gui.py
@@ -12,14 +12,14 @@ gettext.install('yatube','./locale')
 
 class Video:
     
-    def __init__(self,parent_obj,no_pic_image=None):
-        self._np_image = no_pic_image
+    def __init__(self,parent_obj,no=0,no_pic_image=None):
+        self._no        = no
+        self._np_image  = no_pic_image
         self.parent_obj = parent_obj
         self.values()
         self.gui()
     
     def values(self):
-        self._no       = 0
         self._author   = _('Author')
         self._title    = _('Title')
         self._duration = _('Duration')
@@ -110,18 +110,53 @@ class Channel:
         self.values()
         self._name = name
         self.gui()
+        
+    def center(self):
+        max_x = self.widget.winfo_width()
+        max_y = self.widget.winfo_height()
+        sh.log.append ('Channel.center'
+                      ,_('DEBUG')
+                      ,_('Widget sizes: %dx%d') % (max_x,max_y)
+                      )
+        self.widget.update_idletasks()
+        x = self.widget.winfo_screenwidth()/2 - max_x/2
+        y = self.widget.winfo_screenheight()/2 - max_y/2
+        self.widget.geometry("%dx%d+%d+%d" % ((max_x,max_y) + (x, y)))
+        sh.log.append ('Channel.center'
+                      ,_('INFO')
+                      ,_('Set geometry to "%dx%d+%d+%d"') \
+                       % ((max_x,max_y) + (x, y))
+                      )
+    
+    def update_scroll(self):
+        # Do this after adding all videos
+        sg.objs.root().widget.update_idletasks()
+        self._max_y = self.label.widget.winfo_reqheight()
+        self._max_x = self.label.widget.winfo_reqwidth()
+        sh.log.append ('Channel.update_scroll'
+                      ,_('DEBUG')
+                      ,_('Widget sizes: %dx%d') \
+                       % (self._max_x,self._max_y)
+                      )
         self.scrollregion()
         self.scroll2start()
         
     def values(self):
+        self._no     = 0
         self._videos = []
         _np_path     = './nopic.png'
         if sh.File(file=_np_path,Silent=True).Success:
             self._np_image = it.PhotoImage(ig.open(_np_path))
         else:
             self._np_image = None
+        ''' These values set the width and height of the frame that 
+            contains videos and therefore the scrolling region.
+            The default Youtube video picture has the dimensions of
+            196x110, therefore, the channel frame embedding 10 videos
+            will have the height of at least 1100.
+        '''
         self._max_x = 1024
-        self._max_y = 768
+        self._max_y = 1120
         
     def title(self,text=None):
         if text:
@@ -165,13 +200,6 @@ class Channel:
         self.canvas.embed(self.label)
     
     def gui(self):
-        # cur
-        '''
-        self.obj = sg.Top (parent_obj = sg.objs.root()
-                          ,Maximize   = False
-                          ,AutoCenter = False
-                          )
-        '''
         self.obj = sg.SimpleTop(parent_obj=sg.objs.root())
         self.widget = self.obj.widget
         self.title(text=self._name)
@@ -198,9 +226,11 @@ class Channel:
         self.canvas.widget.config(yscrollcommand=self.yscroll.set)
         self.yscroll.config(command=self.canvas.widget.yview)
                           
-    def add(self):
+    def add(self,no=0):
+        self._no = no
         self._videos.append (Video (parent_obj   = self.label
                                    ,no_pic_image = self._np_image
+                                   ,no           = self._no
                                    )
                             )
                             
@@ -219,7 +249,9 @@ class Channel:
                           
     def scroll2start(self,*args):
         self.canvas.widget.xview_moveto(0)
-        self.canvas.widget.yview_moveto(0)
+        # cur
+        #self.canvas.widget.yview_moveto(0)
+        self.canvas.widget.yview_moveto(110*self._no)
         
     def show(self,Lock=True,*args):
         self.obj.show(Lock=Lock)
@@ -233,16 +265,24 @@ if __name__ == '__main__':
     import tkinter as tk
     sg.objs.start()
     channel = Channel(name='Максим Шелков')
-    sg.Geometry(parent_obj=channel).set('700x500')
     for i in range(10):
-        channel.add()
-    sg.objs.root().widget.update_idletasks()
-    for i in range(len(channel._videos)):
-        """
         count = 0
         for k in range(500000):
             count += k
-        """
+        channel.add(no=i)
+        ''' This does not work in 'Channel.__init__' for some reason, 
+        calling this externally
+        ''' 
+        channel.update_scroll()
+    channel.center()
+    sg.objs.root().widget.update_idletasks()
+    '''
+    for i in range(len(channel._videos)):
+        
+        count = 0
+        for k in range(500000):
+            count += k
+        
         video = channel._videos[i]
         video.reset (no       = i
                     ,author   = 'Максим Шелков'
@@ -252,5 +292,9 @@ if __name__ == '__main__':
                     '/home/pete/downloads/hqdefault.jpg'
                     )
         sg.objs.root().widget.update_idletasks()
+    '''
+    # cur
+    #sg.Geometry(parent_obj=channel.obj).set('800x500')
+    #channel.update_scroll()
     channel.show()
     sg.objs.end()
