481342b30558b0918fb87e01521cefcbe98f814d
skl.progs <skl.progs@gmail.com>
2018-03-27 17:21:08 +0300

    utils

--- src/utils.py
+++ src/utils.py
@@ -9,15 +9,29 @@ gettext_windows.setup_env()
 gettext.install('yatube','../resources/locale')
 
 
-class Clone:
+class DB:
     
     def __init__(self,path,clone):
         self._data   = ()
         self._path   = path
         self._clone  = clone
         self.Success = self._clone and sh.File(file=self._path).Success
-        self.connect()
-        self.connectw()
+    
+    def save(self):
+        if self.Success:
+            try:
+                self.dbw.commit()
+            except (sqlite3.DatabaseError,sqlite3.OperationalError):
+                self.Success = False
+                sh.objs.mes ('DB.save'
+                            ,_('WARNING')
+                            ,_('Database "%s" has failed!') % self._clone
+                            )
+        else:
+            sh.log.append ('DB.save'
+                          ,_('WARNING')
+                          ,_('Operation has been canceled.')
+                          )
         
     def connect(self):
         if self.Success:
@@ -26,12 +40,12 @@ class Clone:
                 self.dbc = self.db.cursor()
             except (sqlite3.DatabaseError,sqlite3.OperationalError):
                 self.Success = False
-                sh.objs.mes ('Clone.connect'
+                sh.objs.mes ('DB.connect'
                             ,_('WARNING')
                             ,_('Database "%s" has failed!') % self._path
                             )
         else:
-            sh.log.append ('Clone.connect'
+            sh.log.append ('DB.connect'
                           ,_('WARNING')
                           ,_('Operation has been canceled.')
                           )
@@ -43,12 +57,12 @@ class Clone:
                 self.dbcw = self.dbw.cursor()
             except (sqlite3.DatabaseError,sqlite3.OperationalError):
                 self.Success = False
-                sh.objs.mes ('Clone.connectw'
+                sh.objs.mes ('DB.connectw'
                             ,_('WARNING')
                             ,_('Database "%s" has failed!') % self._clone
                             )
         else:
-            sh.log.append ('Clone.connectw'
+            sh.log.append ('DB.connectw'
                           ,_('WARNING')
                           ,_('Operation has been canceled.')
                           )
@@ -66,12 +80,12 @@ class Clone:
                 self._data = self.dbc.fetchall()
             except (sqlite3.DatabaseError,sqlite3.OperationalError):
                 self.Success = False
-                sh.objs.mes ('Clone.fetch'
+                sh.objs.mes ('DB.fetch'
                             ,_('WARNING')
                             ,_('Database "%s" has failed!') % self._path
                             )
         else:
-            sh.log.append ('Clone.fetch'
+            sh.log.append ('DB.fetch'
                           ,_('WARNING')
                           ,_('Operation has been canceled.')
                           )
@@ -79,7 +93,7 @@ class Clone:
     def create_table(self):
         if self.Success:
             try:
-                # 17 columns by now
+                # 18 columns by now
                 self.dbcw.execute (
                     'create table VIDEOS (\
                      URL       text    \
@@ -99,24 +113,25 @@ class Clone:
                     ,IGNORE    boolean \
                     ,READY     boolean \
                     ,SEARCH    text    \
+                    ,TIMESTAMP float   \
                                                        )'
                                  )
             except (sqlite3.DatabaseError,sqlite3.OperationalError):
                 self.Success = False
-                sh.objs.mes ('Clone.create_table'
+                sh.objs.mes ('DB.create_table'
                             ,_('WARNING')
                             ,_('Database "%s" has failed!') % self._clone
                             )
         else:
-            sh.log.append ('Clone.create_table'
+            sh.log.append ('DB.create_table'
                           ,_('WARNING')
                           ,_('Operation has been canceled.')
                           )
     
-    def copy(self):
+    def fill(self):
         if self.Success:
             if self._data:
-                sh.log.append ('Clone.copy'
+                sh.log.append ('DB.fill'
                               ,_('INFO')
                               ,_('Copy "%s" to "%s"') % (self._path
                                                         ,self._clone
@@ -124,14 +139,18 @@ class Clone:
                               )
                 for row in self._data:
                     try:
+                        search = row[1].lower() + ' ' + row[2].lower()
+                        itime = sh.Time(pattern='%Y-%m-%d %H:%M:%S')
+                        itime._date = row[3]
+                        timestamp = itime.timestamp()
                         row = (row[0],row[1],row[2],row[3],row[4],row[5]
                               ,row[6],row[7],row[8],row[9],row[10]
                               ,row[11],row[12],row[13],row[14],row[15]
-                              ,''
+                              ,search,timestamp
                               )
                         self.dbcw.execute ('insert into VIDEOS values \
                                             (?,?,?,?,?,?,?,?,?,?,?,?,?\
-                                            ,?,?,?,?\
+                                            ,?,?,?,?,?\
                                             )'
                                           ,row
                                           )
@@ -141,18 +160,18 @@ class Clone:
                         self.Success = False
                 # This must be out of the loop
                 if not self.Success:
-                    sh.objs.mes ('Clone.copy'
+                    sh.objs.mes ('DB.fill'
                                 ,_('WARNING')
                                 ,_('Database "%s" has failed!') \
                                 % self._clone
                                 )
             else:
-                sh.log.append ('Clone.copy'
+                sh.log.append ('DB.fill'
                               ,_('WARNING')
                               ,_('Empty input is not allowed!')
                               )
         else:
-            sh.log.append ('Clone.copy'
+            sh.log.append ('DB.fill'
                           ,_('WARNING')
                           ,_('Operation has been canceled.')
                           )
@@ -163,12 +182,12 @@ class Clone:
                 self.dbc.close()
             except (sqlite3.DatabaseError,sqlite3.OperationalError):
                 self.Success = False
-                sh.objs.mes ('Clone.close'
+                sh.objs.mes ('DB.close'
                             ,_('WARNING')
                             ,_('Database "%s" has failed!') % self._path
                             )
         else:
-            sh.log.append ('Clone.close'
+            sh.log.append ('DB.close'
                           ,_('WARNING')
                           ,_('Operation has been canceled.')
                           )
@@ -179,26 +198,63 @@ class Clone:
                 self.dbcw.close()
             except (sqlite3.DatabaseError,sqlite3.OperationalError):
                 self.Success = False
-                sh.objs.mes ('Clone.closew'
+                sh.objs.mes ('DB.closew'
                             ,_('WARNING')
                             ,_('Database "%s" has failed!') % self._clone
                             )
         else:
-            sh.log.append ('Clone.closew'
+            sh.log.append ('DB.closew'
                           ,_('WARNING')
                           ,_('Operation has been canceled.')
                           )
 
 
+
+class Commands:
+    
+    def __init__(self):
+        self._path  = '/home/pete/bin/Yatube/user/yatube.db'
+        self._clone = '/tmp/yatube.db'
+        
+    def alter(self):
+        sh.File(file=self._clone).delete()
+        # Alter DB and add new columns 'SEARCH', 'TIMESTAMP'
+        idb = DB (path  = self._path
+                 ,clone = self._clone
+                 )
+        idb.connect()
+        idb.connectw()
+        idb.fetch()
+        idb.create_table()
+        idb.fill()
+        idb.save()
+        idb.close()
+        idb.closew()
+        
+    def read_random(self):
+        idb = DB (path  = self._path
+                 ,clone = self._clone
+                 )
+        idb.connectw()
+        idb.dbcw.execute ('select AUTHOR,TITLE,SEARCH,TIMESTAMP \
+                           from VIDEOS where AUTHOR=?\
+                          ',('kamikadzedead',)
+                         )
+        data = idb.dbcw.fetchone()
+        if data:
+            print('Author: "%s"' % str(data[0]))
+            print('Title: "%s"' % str(data[1]))
+            print('Search: "%s"' % str(data[2]))
+            print('Timestamp: "%s"' % str(data[3]))
+        else:
+            sh.log.append ('Commands.read_random'
+                          ,_('WARNING')
+                          ,_('Empty input is not allowed!')
+                          )
+        idb.closew()
+
+
 if __name__ == '__main__':
     sh.objs.mes(Silent=1)
-    path  = '/home/pete/bin/Yatube/user/yatube.db'
-    clone = '/tmp/yatube.db'
-    sh.File(file=clone).delete()
-    # Clone DB and add new column 'SEARCH'
-    clone = Clone(path=path,clone=clone)
-    clone.fetch()
-    clone.create_table()
-    clone.copy()
-    clone.close()
-    clone.closew()
+    commands = Commands()
+    commands.read_random()
