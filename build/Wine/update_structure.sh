676bd9d95f1467e788026b09096c90c2a40f8d5a
skl.progs <skl.progs@gmail.com>
2018-10-03 02:27:51 +0300

    Build scripts (Wine)

---
+++ build/Wine/update_structure.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Do not use "verbose" in order to spot errors easily
+
+mkdir -p resources/locale/ru/LC_MESSAGES/ src
+
+# Copy shared resources
+cp -u $HOME/bin/shared/resources/{error,info,question,warning}.gif ./resources/
+
+# Copy Yatube resources
+cp -u $HOME/bin/Yatube/resources/nopic.png ./resources
+cp -u $HOME/bin/Yatube/resources/locale/ru/LC_MESSAGES/yatube.mo ./resources/locale/ru/LC_MESSAGES/yatube.mo
+
+# Copy Yatube Python files
+cp -u $HOME/bin/Yatube/src/{db,gui,logic,tests,yatube}\.py ./src/
+
+# Copy shared Python files
+cp -u $HOME/bin/shared/src/{gettext_windows,shared,sharedGUI}.py ./src/
+
+# Copy Yatube icon
+cp -u $HOME/bin/Yatube/resources/icon_64x64_yatube.gif ./resources/
+
+# (Wine-only) Copy Yatube icon
+cp -u $HOME/bin/Yatube/resources/icon_64x64_yatube.ico ./resources/
+
+rm ./update_structure.sh
+
+ls --color=always .
